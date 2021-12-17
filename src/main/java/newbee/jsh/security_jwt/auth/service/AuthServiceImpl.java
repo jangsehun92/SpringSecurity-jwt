2a2ae5f69ba0161df023a01a7c50453d5ba3b4c2
jangsehun92 <jangsehun92@gmail.com>
2021-12-17 22:18:48 +0900

    [ add ] AuthServiceImpl.java - AuthBlackListRepository

--- src/main/java/newbee/jsh/security_jwt/auth/service/AuthServiceImpl.java
+++ src/main/java/newbee/jsh/security_jwt/auth/service/AuthServiceImpl.java
@@ -8,6 +8,7 @@ import newbee.jsh.security_jwt.auth.dto.request.RequestJwtReissuanceDto;
 import newbee.jsh.security_jwt.auth.dto.request.RequestLoginDto;
 import newbee.jsh.security_jwt.auth.dto.response.ResponseAccessTokenDto;
 import newbee.jsh.security_jwt.auth.dto.response.ResponseTokensDto;
+import newbee.jsh.security_jwt.auth.repository.AuthBlackListRepository;
 import newbee.jsh.security_jwt.auth.repository.AuthRepository;
 import newbee.jsh.security_jwt.config.jwt.JwtProvider;
 
@@ -18,6 +19,7 @@ public class AuthServiceImpl implements AuthService {
     private final JwtProvider jwtProvider;
     private final BCryptPasswordEncoder bCryptPasswordEncoder;
     private final AuthRepository authRepository;
+    private final AuthBlackListRepository authBlackListRepository;
 
     @Override
     public ResponseTokensDto login(final RequestLoginDto dto) {
