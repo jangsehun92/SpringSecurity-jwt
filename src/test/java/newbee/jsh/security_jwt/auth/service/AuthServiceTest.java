3ab30e0b980cefe65ebdb8497c9e231c430b8337
jangsehun92 <jangsehun92@gmail.com>
2021-12-30 22:57:34 +0900

    [ add ] AuthServiceTest

---
+++ src/test/java/newbee/jsh/security_jwt/auth/service/AuthServiceTest.java
@@ -0,0 +1,11 @@
+package newbee.jsh.security_jwt.auth.service;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class AuthServiceTest {
+
+    private AuthService authService;
+    
+}
