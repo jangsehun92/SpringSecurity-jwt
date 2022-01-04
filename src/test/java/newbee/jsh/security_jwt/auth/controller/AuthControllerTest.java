81a11692e2016da647e62c3044a25d9ec401a131
jangsehun92 <jangsehun92@gmail.com>
2022-01-04 21:01:00 +0900

    [ add ] AuthControllerTest

---
+++ src/test/java/newbee/jsh/security_jwt/auth/controller/AuthControllerTest.java
@@ -0,0 +1,11 @@
+package newbee.jsh.security_jwt.auth.controller;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+class AuthControllerTest {
+
+    
+    
+}
