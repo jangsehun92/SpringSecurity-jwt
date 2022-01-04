cbee0b90ff3611022a3619d36af8b60b3251a0b2
jangsehun92 <jangsehun92@gmail.com>
2022-01-04 21:00:23 +0900

    [ edit ] AccountControllerTest unit Test

--- src/test/java/newbee/jsh/security_jwt/account/controller/AccountControllerTest.java
+++ src/test/java/newbee/jsh/security_jwt/account/controller/AccountControllerTest.java
@@ -1,23 +1,31 @@
 package newbee.jsh.security_jwt.account.controller;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.test.web.servlet.setup.StandaloneMockMvcBuilder;
 import org.springframework.web.filter.CharacterEncodingFilter;
 
 import net.minidev.json.JSONObject;
+import newbee.jsh.security_jwt.account.dto.request.RequestAccountCreateDto;
 import newbee.jsh.security_jwt.account.service.AccountService;
 
 @ExtendWith(MockitoExtension.class)
-public class AccountControllerTest {
+class AccountControllerTest {
 
     @InjectMocks
     private AccountController accountController;
@@ -42,7 +50,29 @@ public class AccountControllerTest {
         this.jsonObject = new JSONObject();
     }
 
-    
+    @Test
+    @DisplayName("계정 생성 요청 성공")
+    void createAccountFailByAlreadyEmail() throws Exception{
+        //given
+        final String email = "test@email.com";
+        final String password = "password";
+        final String roleValue = "USER";
+
+        jsonObject.put("email", email);
+        jsonObject.put("password", password);
+        jsonObject.put("roleValue", roleValue);
+
+        final RequestAccountCreateDto dto = objectMapper.readValue(jsonObject.toString(), RequestAccountCreateDto.class);
+
+        //when
+        final MockHttpServletResponse response = mvc.perform(MockMvcRequestBuilders
+                                                                .post("/api/account")
+                                                                .contentType(MediaType.APPLICATION_JSON)
+                                                                .content(objectMapper.writeValueAsString(dto)))
+                                                                .andReturn().getResponse();                                           
+        //then
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+    }
 
     
 }
