62c526f6a689c8ed42b5e22700b0e1ce8b9bf7f1
jangsehun92 <jangsehun92@gmail.com>
2022-01-07 14:20:49 +0900

    [ edit ] JwtProviderTest tokens unit test

--- src/test/java/newbee/jsh/security_jwt/jwt/JwtProviderTest.java
+++ src/test/java/newbee/jsh/security_jwt/jwt/JwtProviderTest.java
@@ -1,6 +1,12 @@
 package newbee.jsh.security_jwt.jwt;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.lang.reflect.Method;
+
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -8,6 +14,7 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import newbee.jsh.security_jwt.auth.repository.AuthBlackListRepository;
 import newbee.jsh.security_jwt.config.CustomUserDetailsService;
 import newbee.jsh.security_jwt.config.jwt.JwtProvider;
+import newbee.jsh.security_jwt.global.util.RandomStringUtil;
 
 @ExtendWith(MockitoExtension.class)
 class JwtProviderTest {
@@ -23,9 +30,37 @@ class JwtProviderTest {
     @BeforeEach
     void before() throws Exception{
         this.jwtProvider = new JwtProvider(customUserDetailsService, authBlackListRepository);
+        Method method = JwtProvider.class.getDeclaredMethod("init");
+        method.setAccessible(true);
+        method.invoke(jwtProvider);
     }
 
-    
+    @Test
+    @DisplayName("accessToken 생성")
+    void createAccessToken() throws Exception{
+        //given
+        final String email = "test@.com";
 
-    
+        //when
+        final String accessToken = jwtProvider.createAccessToken(email);
+
+        //then
+        assertEquals(false, accessToken.isEmpty());
+    }
+
+    @Test
+    @DisplayName("refreshToken 생성")
+    void createRefreshToken() throws Exception{
+        //given
+        final String refreshTokenValue = RandomStringUtil.getRandomString(32);
+
+        //when
+        final String refreshToken = jwtProvider.createRefreshToken(refreshTokenValue);
+
+        //then
+        assertEquals(false, refreshToken.isEmpty());
+    }
+
+
+
 }
