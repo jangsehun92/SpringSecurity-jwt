75251af9c6b163bb95837fdbb06117df78edc409
jangsehun92 <jangsehun92@gmail.com>
2022-01-07 14:33:30 +0900

    [ edit ] jwtProviderTest unit test

--- src/test/java/newbee/jsh/security_jwt/jwt/JwtProviderTest.java
+++ src/test/java/newbee/jsh/security_jwt/jwt/JwtProviderTest.java
@@ -1,6 +1,7 @@
 package newbee.jsh.security_jwt.jwt;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.lang.reflect.Method;
 
@@ -11,6 +12,9 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.security.SignatureException;
 import newbee.jsh.security_jwt.auth.repository.AuthBlackListRepository;
 import newbee.jsh.security_jwt.config.CustomUserDetailsService;
 import newbee.jsh.security_jwt.config.jwt.JwtProvider;
@@ -60,6 +64,71 @@ class JwtProviderTest {
         //then
         assertEquals(false, refreshToken.isEmpty());
     }
+
+    @Test
+    @DisplayName("signingKey가 달라 token 검증 실패")
+    void getClaimsFailBySigningKey() throws Exception{
+        //given
+        final String email = "test@.com";
+        final String accessToken = jwtProvider.createAccessToken(email);
+
+        Method method = JwtProvider.class.getDeclaredMethod("init");
+        method.setAccessible(true);
+        method.invoke(jwtProvider);
+
+        //when
+        final Exception exception = assertThrows(Exception.class, ()->{
+            jwtProvider.getClaims(accessToken);
+        });
+
+        //then
+        assertEquals(exception.getClass(), SignatureException.class);
+    }
+
+    @Test
+    @DisplayName("token 검증 성공 후 Jwt<Claims> 가져오기 성공")
+    void getClaims() throws Exception{
+        //given
+        final String email = "test@.com";
+        final String accessToken = jwtProvider.createAccessToken(email);
+
+        //when
+        Jws<Claims> claims = jwtProvider.getClaims(accessToken);
+
+        //then
+        assertEquals(email, claims.getBody().getSubject());
+    }
+
+    @Test
+    @DisplayName("Subject 가져오기 성공")
+    void getSubject() throws Exception{
+        //given
+        final String email = "test@.com";
+        final String accessToken = jwtProvider.createAccessToken(email);
+
+        //when
+        final String subject = jwtProvider.getSubject(accessToken);
+
+        //then
+        assertEquals(subject, email);
+    }
+
+    @Test
+    @DisplayName("dateValid 성공")
+    void dateValid() throws Exception{
+        //given
+        final String email = "test@.com";
+        final String accessToken = jwtProvider.createAccessToken(email);
+
+        //when
+        boolean result = jwtProvider.dateValid(accessToken);
+
+        //then
+        assertEquals(true, result);
+    }
+
+
+
 
 
 
